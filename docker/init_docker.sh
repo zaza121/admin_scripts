b56704dd8ca89d90616578d3534c7ddfd417181d
Hamza <hamza.yaya@afreetech.com>
2016-07-04 10:09:03 +0100

    [ADD] ADD install script od docker and docker-compose

---
+++ docker/init_docker.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# mise a jour
+sudo apt-get update
+
+# installation de curl
+sudo apt-get install curl
+
+# installation de pip
+sudo apt-get install python-pip python-dev build-essential
+sudo pip install --upgrade pip
+
+#installation de oh my zsh
+sudo apt-get install zsh
+sudo apt-get install git-core
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+# installation de docker
+sudo wget -qO- https://get.docker.com/ | sh
+sudo usermod -aG docker $(whoami)
+
+# installation de docker-compose
+sudo pip install docker-compose
