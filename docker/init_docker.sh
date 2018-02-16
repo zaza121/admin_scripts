2ddacd1309005ccdf39cf42bb8c69f3b0eddc3c6
mr zaza121 <hamza.yaya121@gmail.com>
2018-02-16 11:51:33 +0100

    [IMP] Repond oui a toutes les questions
    1) Les questions bloquent l'execution du script
    
    2) deplace l'installation de zsh en derniere position, son installation termine le script a la fin de l'installation

--- docker/init_docker.sh
+++ docker/init_docker.sh
@@ -4,19 +4,20 @@
 sudo apt-get update
 
 # installation de curl
-sudo apt-get install curl
+sudo apt-get -y install curl
 
 # installation de pip
-sudo apt-get install python-pip python-dev build-essential
+sudo apt-get -y install python-pip python-dev build-essential
 sudo pip install --upgrade pip
 
-#installation de oh my zsh
-sudo apt-get install zsh
-sudo apt-get install git-core
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 # installation de docker
 sudo wget -qO- https://get.docker.com/ | sh
 sudo usermod -aG docker $(whoami)
 
 # installation de docker-compose
 sudo pip install docker-compose
+
+#installation de oh my zsh
+sudo apt-get -y install zsh
+sudo apt-get -y install git-core
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
